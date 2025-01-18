b346c49d3e39ea528bbee87967b8f0158853047d
kda/nova <gastontermine16@gmail.com>
2025-01-18 19:56:05 -0300

    Agregar el cambio visual dark mode y light mode arreglar el ancho del arcor principal del logo y terminar la pagina

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import { IoMoonOutline } from "react-icons/io5";
 import { FaSun } from "react-icons/fa6";
 import { CiSearch } from "react-icons/ci";
 import { useState } from "react";
-import './style.css'
 import { useQuery } from "react-query";
 import { DictionaryAnswer, DictionaryAnswerWithError } from "./interface";
 import { ResultsNotFound, GenericError } from "./ERRORES/errors";
-import ShowResults from "./RESULTS/showResults";
+import ShowResults from "./RESULTS/ShowResults";
+
+import './style.css'
+import './CSS/app.css'
+
 
 export default function App() {
 
     const [sourceOfLetter, setSourceOfLetter] = useState<string>('serif')
+    const [backgroundTheme, setBackgroundTheme] = useState<string>('light')
 
     let timeBeforeSearching: ReturnType<typeof setTimeout>;
     const [keyword, setKeyword] = useState<string | undefined>(undefined)
@@ -68,46 +72,78 @@ export default function App() {
     });
 
     return (<>
-        <header style={{ fontFamily: `${sourceOfLetter}` }}>
-            <a href="/" rel="noopener noreferrer">
-                <GiBlackBook />
+        <header
+            className="app-header"
+            style={{ fontFamily: `${sourceOfLetter}` }}>
+            <a
+                className="app-header__ancla"
+                href="/" rel="noopener noreferrer">
+                <GiBlackBook className="app-header__ancla__icon" />
                 Free Dictionary API
             </a>
 
-            <div>
-                <select onChange={(e) => setSourceOfLetter(e.target.value)}>
-                    <option value="serif">Serif</option>
-                    <option value="sans-serif">Sans Serif</option>
-                    <option value="monospace">Monospace</option>
-                </select>
-                <RiLetterSpacing2 />
-            </div>
+            <div className="app-header__change">
+                <div className="app-header__changeLetter">
+                    <select
+                        className="app-header__changeLetter__select"
+                        onChange={(e) => setSourceOfLetter(e.target.value)}>
+                        <option
+                            className="app-header__changeLetter__select__letter"
+                            value="serif" style={{ fontFamily: 'serif' }}>Serif</option>
 
-            <div>
-                <label>
-                    <input type="radio" name="option" value="option1" />
-                </label>
+                        <option
+                            className="app-header__changeLetter__select__letter"
+                            value="sans-serif" style={{ fontFamily: 'sans-serif' }}>Sans Serif</option>
 
-                <label>
-                    <input type="radio" name="option" value="option2" />
-                </label>
+                        <option
+                            className="app-header__changeLetter__select__letter"
+                            value="monospace" style={{ fontFamily: 'monospace' }}>Monospace</option>
+                    </select>
+                    <RiLetterSpacing2 className="app-header__changeLetter__icon" />
+                </div>
 
-                <IoMoonOutline />
-                <FaSun />
+                <div className="app-header__changeTema">
+                    <input
+                        style={{ opacity: '0', cursor: 'pointer' }}
+                        type="radio"
+                        id="dark"
+                        name="theme"
+                        value="dark"
+                        onChange={(e) => setBackgroundTheme(e.target.value)}
+                    />
+                    <input
+                        style={{ opacity: '0', cursor: 'pointer' }}
+                        type="radio"
+                        id="light"
+                        name="theme"
+                        value="light"
+                        onChange={(e) => setBackgroundTheme(e.target.value)}
+                    />
+                    <div className={`topicIndicator ${backgroundTheme}`}></div>
+                </div>
+
+                {backgroundTheme === 'light' ? <FaSun className="app-header__light-icon" /> : <IoMoonOutline className="app-header__dark-icon" />}
+
             </div>
 
         </header>
 
-        <main style={{ fontFamily: `${sourceOfLetter}` }}>
-            <label>
-                <input type="search" onChange={(e) => doublingSetWordReference(e.target.value)} />
-                <CiSearch />
-            </label>
+        <main
+            className="app-main"
+            style={{ fontFamily: `${sourceOfLetter}` }}>
+                
+            <div className="app-main__label-search">
+                <input
+                    className="app-main__input-search"
+                    style={{ fontFamily: `${sourceOfLetter}` }}
+                    type="search" onChange={(e) => doublingSetWordReference(e.target.value)} />
+                <CiSearch className="app-main__input-search__icon" />
+            </div>
 
             {
-                !result
+                !result && !isError
                     ?
-                    <span>¡Bienvenido! Busca una palabra.</span>
+                    <span className="app-main__welcome">¡Bienvenido! Busca una palabra.</span>
                     :
                     <ShowResults
                         isError={isError}
--- src/RESULTS/showResults.tsx
+++ src/RESULTS/showResults.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DictionaryAnswer, DictionaryAnswerWithError } from "../interface"
 import { ImSearch } from "react-icons/im";
+import '../CSS/showResults.css'
 
 interface Props {
     result: DictionaryAnswer[] | DictionaryAnswerWithError | undefined,
@@ -11,16 +12,17 @@ interface Props {
 const ShowResults: React.FC<Props> = ({ result, isLoading, isError }) => {
 
     const [linkAudio, setLinkAudio] = useState<string>('')
+    const audioRef = useRef<HTMLAudioElement>(null)
 
     useEffect(() => {
-        if (Array.isArray(result)) setLinkAudio(result[0].phonetics[0].audio)
+        if (Array.isArray(result)) setLinkAudio(result[0]?.phonetics[0]?.audio)
     }, [result])
 
     if (isLoading) {
         return (
-            <div>
-                <ImSearch />
-                <span>Buscando...</span>
+            <div className="ShowResults__isLoading">
+                <ImSearch className="ShowResults__isLoading__icon" />
+                <span className="ShowResults__isLoading__textMessage">Buscando...</span>
             </div>
         );
     }
@@ -28,76 +30,115 @@ const ShowResults: React.FC<Props> = ({ result, isLoading, isError }) => {
     if (result) {
         if ('title' in result) {
             return (
-                <div>
-                    <h3>{result.title}</h3>
-                    <p>{result.message}</p>
-                    <p>{result.resolution}</p>
+                <div className="ShowResults__resultNotFound">
+                    <h3 className="ShowResults__resultNotFound__title">{result.title}</h3>
+                    <p className="ShowResults__resultNotFound__message">{result.message}</p>
+                    <p className="ShowResults__resultNotFound__resolution">{result.resolution}</p>
                 </div>
             );
 
         } else {
 
             return (
-                <ul>
-                    {result.map((word, index) => (
-                        <li key={index}>
-                            <section>
-                                <div>
-                                    <span>{word.word}</span>
+                <ul className="ShowResults__results">
+                    {result?.map((word, index) => (
+                        <li
+                            className="ShowResults__results__item"
+                            key={index}>
+                            <section className="ShowResults__results__item__word">
+                                <div className="ShowResults__results__item__word__content">
+                                    <span className="ShowResults__results__item__word__content__world-span">{word.word}</span>
 
-                                    <select onChange={e => setLinkAudio(e.target.value)}>
+                                    <select
+                                        className="ShowResults__results__item__word__content__select_world-scund"
+                                        onChange={e => {
+                                            const selectedValue = e.target.value;
+
+                                            if (selectedValue) {
+                                                setLinkAudio(selectedValue);
+                                            } else {
+                                                setLinkAudio('')
+                                            }
+                                        }}
+                                    >
                                         {word.phonetics.map((item, index) => (
-                                            <option key={index} value={item.audio}>{item.text}</option>
+                                            item.text && <option
+                                                className="ShowResults__results__item__word__content__world-scund"
+                                                key={index} value={item.audio}>{item.text}</option>
                                         ))}
                                     </select>
                                 </div>
 
-                                <audio src={linkAudio} controls></audio>
+                                <audio
+                                    ref={audioRef}
+                                    src={linkAudio}></audio>
+                                <button
+                                    className={`ShowResults__results__item__word__audio ${linkAudio.length <= 0 && 'no-audio'}`}
+                                    onClick={() => { if (linkAudio.length > 0) audioRef.current?.play() }}
+                                    type="button">▶</button>
                             </section>
 
-                            <section>
-                                <ul>
+                            <section className="ShowResults__results__item__complements">
+                                <ul className="ShowResults__results__item__complements__list">
                                     {word.meanings.map((item, index) => (
-                                        <li key={index}>
-                                            <h4>{item.partOfSpeech}</h4>
+                                        <li
+                                            className="ShowResults__results__item__complements__list__item"
+                                            key={index}>
+                                            <h4 className="ShowResults__results__item__complements__list__item__partOfSpeech">{item.partOfSpeech}</h4>
 
-                                            <ul>
+                                            <ul className="ShowResults__results__item__complements__list__item__definitions">
                                                 {item.definitions.map((def, index) => (
-                                                    <li key={index}>
-                                                        <p>
-                                                            <span>Definición:</span> <br />
-                                                            {def.definition}
-                                                        </p>
+                                                    <li
+                                                        className="ShowResults__results__item__complements__list__item__definitions__def"
+                                                        key={index}>
 
-                                                        <p>
-                                                            <span>Ejemplo:</span> <br />
+                                                        {def.definition && <p className="ShowResults__results__item__complements__list__item__definitions__def__p">
+                                                            <span className="ShowResults__results__item__complements__list__item__definitions__def__p__title">Definición:</span> <br />
+                                                            {def.definition}
+                                                        </p>}
+
+                                                        {def.example && <p className="ShowResults__results__item__complements__list__item__definitions__def__p">
+                                                            <span className="ShowResults__results__item__complements__list__item__definitions__def__p__title">Ejemplo:</span> <br />
                                                             {def.example}
-                                                        </p>
+                                                        </p>}
+
                                                     </li>
                                                 ))}
                                             </ul>
 
-                                            <ul>
+                                            <ul className="ShowResults__results__item__complements__list__item__list__synonyms">
+                                                {item.synonyms.length > 0 && <span className="ShowResults__results__item__complements__list__item__list__synonyms__word__title">Sínonimos: </span>}
                                                 {item.synonyms.map((word, index) => (
-                                                    <li key={index}>{word}</li>
+                                                    <li
+                                                        className="ShowResults__results__item__complements__list__item__list__synonyms__word"
+                                                        key={index}>
+                                                        {word}
+                                                    </li>
                                                 ))}
                                             </ul>
 
-                                            <ul>
+                                            <ul className="ShowResults__results__item__complements__list__item__list__synonyms">
+                                                {item.antonyms.length > 0 && <span className="ShowResults__results__item__complements__list__item__list__synonyms__word__title">Antónimos: </span>}
                                                 {item.antonyms.map((word, index) => (
-                                                    <li key={index}>{word}</li>
+                                                    <li
+                                                        className="ShowResults__results__item__complements__list__item__list__synonyms__word"
+                                                        key={index}>
+                                                        {word}
+                                                    </li>
                                                 ))}
                                             </ul>
                                         </li>
                                     ))}
                                 </ul>
 
-                                <span>Fuente:</span>
+                                <span className="ShowResults__results__item__complements__font">Fuente:</span>
                                 {word.sourceUrls.map((link, index) => (
                                     <a
+                                        className="ShowResults__results__item__complements__font__arcor"
                                         key={index}
                                         target="_blank" href={link}>{link}</a>
                                 ))}
+
                             </section>
                         </li>
                     ))}
@@ -107,8 +148,11 @@ const ShowResults: React.FC<Props> = ({ result, isLoading, isError }) => {
     }
 
     if (isError) {
-        return <div>Error</div>;
+        return <span style={{ color: 'red' }}>
+            Ocurrió un error inesperado. Vuelve a intentarlo.
+        </span>;
     }
+
 
     return undefined;
 };
